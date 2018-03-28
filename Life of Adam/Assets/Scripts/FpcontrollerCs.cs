ea877a8543f9173b670ac66b6c2cf2c3f4ec216d
Noaman Khalil <caedriel@live.com>
2018-03-28 20:19:38 +0400

    #1
    initialCommit*

---
+++ Life of Adam/Assets/Scripts/FpcontrollerCs.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpcontrollerCs : MonoBehaviour
+{
+	public GameObject camera;
+	public float inputDelay;
+	public float rotationSpeed;
+	public float Xaxis, YAxis ;
+
+	public float straffeVel;
+	public float runVel = 36f;
+	public float jumpVel = 0.2f;
+
+	private float forwardVel;
+	private Vector3 velocity;
+	private Vector3 IVeloctiy;
+	private float walkVel = 8f;
+	Rigidbody rb;
+	float forwardInput, straffeInput;
+	private float tempStamina;
+	bool isPlaying;
+	bool canJump;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		forwardInput = straffeInput = 0;
+		isPlaying = false;
+
+		forwardVel = walkVel;
+	}
+
+	void Update()
+    { 
+		Run();
+
+		forwardInput = Input.GetAxis("Vertical") * forwardVel;
+		straffeInput = Input.GetAxis("Horizontal") * forwardVel;
+//		Debug.Log(straffeInput);
+		//transform.Translate(straffeInput, 0, forwardInput);
+		velocity = new Vector3(straffeInput, 0, forwardInput);
+		//velocity = transform.localToWorldMatrix * velocity ;
+		velocity = transform.TransformDirection(velocity);
+		velocity.y = rb.velocity.y; 
+
+	     rb.velocity =  velocity;
+
+
+		if (canJump & Input.GetKeyUp(KeyCode.Space))
+		{
+			rb.AddForce(new Vector3(0, jumpVel, 0), ForceMode.Impulse);
+			canJump = false;
+		}
+
+	}
+
+	void Run()
+	{
+        if (Input.GetKey(KeyCode.LeftShift) && tempStamina <= 0)
+		{
+			forwardVel = runVel ;
+		}
+		if (Input.GetKeyUp(KeyCode.LeftShift))
+		{
+			forwardVel = walkVel;
+		}
+	}
+
+
+	void OnCollisionStay(Collision other)
+	{
+		canJump = true;
+    }
+
+}
---
+++ Life of Adam/Assets/Scripts/PickUpCs.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PickUpCs : MonoBehaviour
+{
+    GameObject mainCam;
+    bool isCarrying;
+    GameObject carriedObj;
+    public float dist;
+    public float smooth;
+
+	// Use this for initialization
+	void Start ()
+    {
+		if (mainCam == null)
+        {
+            mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isCarrying)
+        {
+            carry(carriedObj);
+            checkdrop();
+        }
+        else
+        {
+            pickup();
+        }
+	}
+
+    void carry (GameObject o)
+    {
+
+    }
+    void pickup ()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            float x = Screen.width / 2;
+            float y = Screen.height / 2;
+
+            Ray ray = mainCam.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+            RaycastHit r;
+            if (Physics.Raycast(ray , out r))
+            {
+                // dummy script to access the object 
+                PickMeCs p = r.collider.GetComponent<PickMeCs>();
+               if ( r.collider.tag.Equals("Pick"))
+                {
+                    isCarrying = true;
+                    carriedObj = p.gameObject;
+                    p.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                }
+            }
+
+        }
+    }
+    void checkdrop ()
+    {
+        if (Input.GetKeyDown (KeyCode.E))
+        {
+            dropObj();
+        }
+    }
+
+    void dropObj ()
+    {
+        isCarrying = false;
+        carriedObj.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        carriedObj = null;
+      }
+}
