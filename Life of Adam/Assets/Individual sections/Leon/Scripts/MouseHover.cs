a992e5b9913752a6a45482d68a23d56b9f556d45
Leon <p.fernandes@saedubai.com>
2018-04-28 18:21:53 +0400

    UI changes video swaps folder organization

---
+++ Life of Adam/Assets/Individual sections/Leon/Scripts/MouseHover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseHover : MonoBehaviour {
+
+    public GameObject image;
+    
+    
+
+     public void OnMouseOver()
+    {
+        image.SetActive(true);
+        
+        
+
+    }
+
+     public void OnMouseExit()
+    {
+        image.SetActive(false);
+    }
+}
---
+++ Life of Adam/Assets/Individual sections/Leon/Scripts/Scene.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+///LoadScene , used to control scenes andor quit d
+
+public class Scene  : MonoBehaviour
+{
+    // Use this for initialization
+    bool mytimeScale = true;
+    public GameObject aboutPanel;
+    public GameObject mainPanel;
+    public void LoadScene(string str)
+    {
+        SceneManager.LoadScene(str);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void pause()
+    {
+
+        Time.timeScale = 0;
+    }
+    public void unPause()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void ToVideoScene()
+    {
+        SceneManager.LoadScene("VideoScene");
+    }
+
+    public void AboutPanel()
+    {
+        aboutPanel.gameObject.SetActive(true);
+        mainPanel.gameObject.SetActive(false);
+    }
+
+    public void MainPanel()
+    {
+        mainPanel.gameObject.SetActive(true);
+        aboutPanel.gameObject.SetActive(false);
+    }
+
+
+
+    
+
+
+}
