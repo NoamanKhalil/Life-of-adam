d91cb4489af752911c6f3e196a3228728dc96df1
Leon <p.fernandes@saedubai.com>
2018-05-07 20:42:32 +0400

    T area start up

---
+++ Life of Adam/Assets/Individual sections/Leon/Scripts/ObjectDestroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ObjectDestroy : MonoBehaviour {
+
+    public AudioClip[] clips;
+    public int i;
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "object")
+        {
+            Destroy(other.gameObject);
+            
+            
+        }
+    }
+}
