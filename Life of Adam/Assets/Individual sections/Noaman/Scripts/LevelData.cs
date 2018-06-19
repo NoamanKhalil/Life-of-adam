fea90e7994039a84b5b20d9e6f28325ad92db86f
Noaman Khalil <caedriel@live.com>
2018-06-19 16:23:57 +0400

    #11
    Migration to MACos

---
+++ Life of Adam/Assets/Individual sections/Noaman/Scripts/LevelData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class LevelData : MonoBehaviour {
+
+
+    public bool isDayOne;
+    public bool isDayTwo;
+    public bool isDayThree;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
