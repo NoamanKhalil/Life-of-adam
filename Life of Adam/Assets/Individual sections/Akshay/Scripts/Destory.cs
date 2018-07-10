3c46ed525d5df1765df2ecdf74d7118202f35af8
Akshay Pancholi <willreplyasap@gmail.com>
2018-07-10 12:00:53 +0400

    Pushing Complete White Box - Happy day 2

---
+++ Life of Adam/Assets/Individual sections/Akshay/Scripts/Destory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destory : MonoBehaviour {
+    
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player") 
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Life of Adam/Assets/Individual sections/Akshay/Scripts/PlatformCs.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlatformCs : MonoBehaviour
+{
+
+    public Transform[] pathToFollow;
+    public int posPoint;
+    public float speed;
+    public float pauseTimer;
+    public float maxTime;
+    // Use this for initialization
+    void Start()
+    {
+        pathToFollow = GameObject.Find("wayPointParent").GetComponentsInChildren<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        patrol();
+    }
+
+
+    void patrol()
+    {
+
+        if (posPoint < pathToFollow.Length)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pathToFollow[posPoint].position, Time.deltaTime * speed);
+        }
+
+        if (transform.position == pathToFollow[posPoint].position)
+        {
+            pauseTimer -= Time.deltaTime;
+            if (pauseTimer <= 0)
+            {
+
+                if (posPoint == pathToFollow.Length -1)
+                {
+                    posPoint = 0;
+                    pauseTimer = maxTime;
+
+                    return; 
+                }
+
+                posPoint++;
+
+            }
+
+        }
+    }
+
+}
