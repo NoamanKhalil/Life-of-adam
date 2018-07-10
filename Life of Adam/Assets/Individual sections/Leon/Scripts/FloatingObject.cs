16ccb33bb23f59106ef9f124b5008c792bd1de60
Leon <p.fernandes@saedubai.com>
2018-07-10 11:42:14 +0400

    T-area object set up

---
+++ Life of Adam/Assets/Individual sections/Leon/Scripts/FloatingObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingObject : MonoBehaviour {
+
+    Vector3 startPosition;
+    public float intensity;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+
+        float x = startPosition.x;
+        float y = intensity * Mathf.Sin(Time.time) + startPosition.y;
+        float z = startPosition.z;
+
+        transform.position = new Vector3(x, y, z);
+    }
+
+
+}
