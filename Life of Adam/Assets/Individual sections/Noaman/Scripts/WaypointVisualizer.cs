2cdb0758a7f33a98a89d403bcaf510e47257acbd
NOAMAN Khalil <caedriel@live.com>
2018-07-14 18:57:49 +0400

    #22
    Added a new recasting to find player*

---
+++ Life of Adam/Assets/Individual sections/Noaman/Scripts/WaypointVisualizer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointVisualizer : MonoBehaviour 
+{
+    [SerializeField]
+	float sphereRadius;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDrawGizmos()
+	{
+		// Draw a yellow sphere at the transform's position
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(transform.position, sphereRadius);
+	}
+
+
+	}
