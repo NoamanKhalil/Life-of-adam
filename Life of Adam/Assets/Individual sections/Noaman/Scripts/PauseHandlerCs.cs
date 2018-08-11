a1815cd8a3d421c546926d34137cb8f72e8492a7
NOAMAN Khalil <caedriel@live.com>
2018-08-11 16:55:37 +0400

    #35
    Saving works better (Animation)*

--- Life of Adam/Assets/Individual sections/Noaman/Scripts/PauseHandlerCs.cs
+++ Life of Adam/Assets/Individual sections/Noaman/Scripts/PauseHandlerCs.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class PauseHandlerCs : MonoBehaviour
 {
+	public CanvasGroup can;
+	public float smooth;
 	public GameObject myCanvas;
 	public bool isPaused;
+	private bool isIsRunningCoroutine;
 
 	// Use this for initialization
 	void Start ()
 	{
-		myCanvas.SetActive(false);
-
+		//myCanvas.SetActive(false);
+		isPaused = false;
+		can.alpha = 0;
 	}
 	
 	// Update is called once per frame
@@ -19,35 +23,81 @@ public class PauseHandlerCs : MonoBehaviour
 	{
 		if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
 		{
-			if (!isPaused)
+			if (isPaused)
 			{
-				Cursor.visible = true;
-		        Cursor.lockState = CursorLockMode.None;
-				isPaused = true;
-				myCanvas.SetActive(true);
-				Time.timeScale = 0;
-				return;
-			}
-			else if (isPaused)
-			{
-				Cursor.visible = false;
-				Cursor.lockState = CursorLockMode.Locked;
 				isPaused = false;
-				myCanvas.SetActive(false);
-				Time.timeScale = 1;
-				return;
+			}
+			else
+			{
+				isPaused = true;
 			}
 		}
-		
+		//if is pasued is flase
+	    if (!isPaused)
+		{
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
+            StopAllCoroutines();
+			StartCoroutine(uiFadein(can));
+			if (can.alpha <=1)
+			{
+				Time.timeScale = 1;
+				//myCanvas.SetActive(false);
+			}
+
+			return;
+
+
+		}
+		//is is pasued is true
+		else if (isPaused)
+		{
+			//myCanvas.SetActive(true);
+			Cursor.visible = true;
+	        Cursor.lockState = CursorLockMode.None;
+            StopAllCoroutines();
+
+			StartCoroutine(uiFadeout(can));
+			if (can.alpha>=1)
+			{
+				Time.timeScale = 0;
+			}
+			return;
+		}
 	}
 
 
+	private IEnumerator uiFadein(CanvasGroup cg)
+	{
+		isIsRunningCoroutine = true;
+		while (can.alpha >= 0)
+		{
+			can.alpha -= Time.deltaTime / smooth;
+
+		    isIsRunningCoroutine = false;
+			yield return null;
+		}
+
+		isIsRunningCoroutine = false;
+		yield return null;
+
+
+	}
+	private IEnumerator uiFadeout(CanvasGroup cg)
+	{
+		isIsRunningCoroutine = true;
+		while (can.alpha <= 1)
+		{
+			can.alpha += Time.deltaTime / smooth;
+		    isIsRunningCoroutine = false;
+			yield return null;
+		}
+		isIsRunningCoroutine = false;
+		yield return null;	
+	}
+
 	public void unPause()
-	{ 
-		Cursor.visible = false;
-		Cursor.lockState = CursorLockMode.Locked;
-	    isPaused = false;
-		myCanvas.SetActive(false);
-		Time.timeScale = 1;
+	{
+		isPaused = false;
 	}
 }
