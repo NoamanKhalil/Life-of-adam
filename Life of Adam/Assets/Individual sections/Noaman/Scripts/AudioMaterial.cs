f0b0bf612f866fc730233f7087c1596d0cec6ca1
NOAMAN Khalil <caedriel@live.com>
2018-08-29 21:24:05 +0400

    #40
    Shapes replaced with cubes *

---
+++ Life of Adam/Assets/Individual sections/Noaman/Scripts/AudioMaterial.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Sounds
+{
+    North, East, South, West
+};
+public class AudioMaterial : MonoBehaviour 
+{
+    [Header("Audio for this object ")]
+    [SerializeField]
+    private Sounds currentSound;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
